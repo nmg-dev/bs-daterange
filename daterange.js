602c0797e3e7f8fc410e2cb9c203e225d8637f56
yg.song@nextmediagroup.co.kr <insaint03@gmail.com>
2019-02-14 22:59:07 +0900

    tmp with preset

--- daterange.js
+++ daterange.js
@@ -1,18 +1,50 @@
 $.fn.daterange = (function(options) {
+	const DATE_FORMAT = 'YYYY-MM-DD';
+	const MONTH_FORMAT = 'YYYY-MM';
+	const TITLE_FORMAT = 'MMM.YYYY';
+
+	const WEEKDAYS = ['sun','mon','tue','wed','thu','fri','sat'];
+	const PRESET_RANGES = {
+		'this_week': (till)=>moment(till).weekday(this._options.weekStart).format(DATE_FORMAT),
+		'this_month': (till)=>moment(till).date(1).format(DATE_FORMAT),
+		'this_year': (till)=>moment(till).dayOfYear(1).format(DATE_FORMAT),
+		'7_days': (till)=>moment(till).add(-7, 'day').format(DATE_FORMAT),
+		'4_weeks': (till)=>moment(till).add(-4, 'week').format(DATE_FORMAT),
+		'30_days': (till)=>moment(till).add(-30, 'day').format(DATE_FORMAT),
+		'3_months': (till)=>moment(till).add(-3, 'month').format(DATE_FORMAT),
+		'12_weeks': (till)=>moment(till).add(-12, 'week').format(DATE_FORMAT),
+		'90_days': (till)=>moment(till).add(-90, 'day').format(DATE_FORMAT),
+		'a_year': (till)=>moment(till).add(-1, 'year').format(DATE_FORMAT),
+	};
+	const _TODAY = moment().format(DATE_FORMAT);
+
 	const DEFAULT_OPTIONS = {
 		calendar: '2019-02',
 		ranged: {
-			from: '2019-01-29',
-			till: '2019-02-15',
+			from: null,
+			till: _TODAY,
 		},
-		selected: null,
+		selected: _TODAY,
+		focused: 'from',
 		moveMonth: true,
 		moveYear: true,
-		titleFormat: 'YYYY.MMM',
+		titleFormat: TITLE_FORMAT,
 		weekStart: 0,
-		presets: [],
+		presets: [
+			/* { 
+				- title: label name,
+				- desc: short description,
+				- range: function(till :string) => return from :string
+			}*/
+			{ title: '이번달', desc: '월초 ~ 현재', range: PRESET_RANGES['this_month'] },
+			{ title: '일 개월', desc: '30일', range: PRESET_RANGES['30_days'] },
+			{ title: '분기', desc: '3개월 (1/4년)', range: PRESET_RANGES['3_months'] },
+			{ title: '올해', desc: '연초 ~ 현재', range: PRESET_RANGES['this_year'] },
+			{ title: '일 년', desc: '1년', range: PRESET_RANGES['a_year'] },
+		],
 	}; 
-	const WEEKDAYS = ['sun','mon','tue','wed','thu','fri','sat'];
+	
+	this.__preset_ranges = PRESET_RANGES;
 	// console.log(this, options);
 
 	this._options = Object.assign(DEFAULT_OPTIONS, this[0].dataset, options);
@@ -21,6 +53,7 @@ $.fn.daterange = (function(options) {
 
 	let _onClickControl = function(ev) {
 		let ctrl = ev.target.tagName != 'I' ? ev.target : ev.target.parentElement;
+		console.log(ev, ctrl);
 		// on click title control
     	if(ctrl.classList.contains('bs-daterange-control')) {
     		let unit = ctrl.classList.contains('bs-daterange-year') ? 'year' : 'month';
@@ -28,16 +61,33 @@ $.fn.daterange = (function(options) {
 
     		this._options.calendar = moment(this._options.calendar)
     			.add(unit, direction)
-    			.format('YYYY-MM');
-    		this._drawControls();
+    			.format(MONTH_FORMAT);
     	}
-
     	// on click calendar
     	if(ctrl.classList.contains('bs-daterange-cellbody')) {
-    		console.log(ctrl.dataset.date);
+    		if(this._options.focused) {
+    			this._options.ranged[this._options.focused] = ctrl.dataset.date;
+    			this._options.focused = null;
+    		}
     		this._options.selected = ctrl.dataset.date;
-    		this._drawControls();
     	}
+    	// on click input
+    	if(ctrl.classList.contains('bs-daterange-period')) {
+    		this._options.selected = null;
+    		this._options.focused = ctrl.name;
+    	}
+    	// on click preset
+    	if(ctrl.hasAttribute('preset-idx')) {
+    		if(!this._options.ranged.till)
+    			this._options.ranged.till = moment().format(DATE_FORMAT);
+    		let pidx = parseInt(ctrl.getAttribute('preset-idx'));
+    		this._options.ranged.from = this._options.presets[pidx].range(this._options.ranged.till);
+    		this._options.selected = this._options.ranged.from;
+    		this._options.focused = null;
+    		this._options.presets[pidx].selected = true;
+    	} 
+
+    	this._drawControls();
     	ev.preventDefault();
     	ev.stopPropagation();
 	}
@@ -72,13 +122,15 @@ $.fn.daterange = (function(options) {
 	    dcursor.date(1);
 	    dcursor.add('day', this._options.weekStart-dcursor.weekday());
 	    dcursor.hour(0);dcursor.minute(0);dcursor.second(0);dcursor.millisecond(2);
-		let the_month = dcursor.format('YYYY-MM');
+		let the_month = dcursor.format(MONTH_FORMAT);
 
 		let range_from = this._options.ranged && this._options.ranged.from ? moment(this._options.ranged.from) : null;
 		let range_till = this._options.ranged && this._options.ranged.till ? moment(this._options.ranged.till) : null;
 		let inRange = null;
-		range_from.hour(0);range_from.minute(0);range_from.second(0);range_from.millisecond(0);
-		range_till.hour(23);range_till.minute(59);range_till.second(59);range_till.millisecond(999);
+		if(range_from && range_till) {
+			range_from.hour(0);range_from.minute(0);range_from.second(0);range_from.millisecond(0);
+			range_till.hour(23);range_till.minute(59);range_till.second(59);range_till.millisecond(999);
+		}
 
 	    do {
 	    	let row = tblBody.append('<tr></tr>');
@@ -88,8 +140,8 @@ $.fn.daterange = (function(options) {
 
 				// month class
 				let wc = 'bs-daterange-the-month';
-				the_month = dcursor.format('YYYY-MM');
-				let the_day = dcursor.format('YYYY-MM-DD');
+				the_month = dcursor.format(MONTH_FORMAT);
+				let the_day = dcursor.format(DATE_FORMAT);
 				if(the_month!=this._options.calendar) {
 					wc = dcursor.isBefore(_dbase) ? 'bs-daterange-prev-month' : 'bs-daterange-next-month';
 				}
@@ -102,7 +154,7 @@ $.fn.daterange = (function(options) {
 				if(range_from && range_from.isBefore(dcursor) && range_till.isAfter(dcursor))
 					wc += ' in-range';
 				
-				row.append(`<td class="bs-daterange-cellbody weekday-${wd} ${wc}" data-date="${dcursor.format('YYYY-MM-DD')}">
+				row.append(`<td class="bs-daterange-cellbody weekday-${wd} ${wc}" data-date="${dcursor.format(DATE_FORMAT)}">
 					${dcursor.date()}
 				</td>`);
 	    		dcursor.add('day', 1);
@@ -110,19 +162,32 @@ $.fn.daterange = (function(options) {
 	    } while(the_month==this._options.calendar);
 
 	    // setup ranged from/till
-	    this.find(`td[data-date="${range_from.format('YYYY-MM-DD')}"]`).addClass('period-from');
-	    	// .html(`<span class="period-from">${range_from.date()}</span>`);
-	    this.find(`td[data-date="${range_till.format('YYYY-MM-DD')}"]`).addClass('period-till');
-	    	// .html(`<span class="period-till">${range_till.date()}</span>`);
+	    if(range_from && range_till) {
+		    this.find(`td[data-date="${range_from.format(DATE_FORMAT)}"]`).addClass('period-from');
+		    	// .html(`<span class="period-from">${range_from.date()}</span>`);
+		    this.find(`td[data-date="${range_till.format(DATE_FORMAT)}"]`).addClass('period-till');
+		    	// .html(`<span class="period-till">${range_till.date()}</span>`);
+	    }
 
-	    this._selector.append(`<div class="bs-daterange-controls">
-	    	<div class="input-group">
-	    		<input class="form-control bs-daterange-period bs-daterange-from" type="date" readonly>
-	    		<span class="input-text"> - </span>
-	    		<input class="form-control bs-daterange-period bs-daterange-till" type="date" readonly>
+	    let presets = this._options.presets.map(
+	    	(ps, idx)=>`<a href="#" class="list-group-item list-group-item-action flex-column align-items-start bs-daterange-preset-item ${ps.selected?'active':''}"  preset-idx=${idx}>
+    			<div class="d-flex w-100 justify-content-between" preset-idx=${idx}>
+	      			<h5 preset-idx=${idx}>${ps.title}</h5>
+	      			<small preset-idx=${idx}>${ps.desc}</small>
+    			</div>
+    		</a>`) || [];
+	   	this._selector.append(`<div class="list-group bs-daterange-presets">${presets.join(' ')}</div>`);
+	    this._selector.prepend(`<div class="bs-daterange-controls">
+	    	<div class="form-group">
+	    		<input class="form-control bs-daterange-period bs-daterange-from" name="from" type="date" value="${this._options.ranged.from}" readonly>
+	    		<input class="form-control bs-daterange-period bs-daterange-till" name="till" type="date" value="${this._options.ranged.till}" readonly>
 	    	</div>
-	    	<ul class="dropdown-menu bs-daterange-presets"></ul>
 	    </div>`);
+
+	    if(this._options.focused) {
+	    	this._selector.find(`input[name="${this._options.focused}"]`).addClass('focused');
+	    }
+
 
 	    if(initializing) {
 		    this.click(_onClickControl.bind(this));    	
